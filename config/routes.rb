2d245e5e82eafac423fe855834a99e16b6d04e05
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-18 15:23:51 +0000

    created user model, table, controlelr

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/create'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
+  get 'users/show'
   root to: 'clothing_items#home'
   resources :clothing_items, only: [:index, :show] do
     resources :reservations, except: [:destroy]
