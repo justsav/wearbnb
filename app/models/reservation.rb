6468b7f121bce92066c79448493aa0ad7bc9af99
Justin Savage <justin.savage@live.com>
2019-11-18 15:21:33 +0000

    Revert "Reservation model and routes changes"

--- app/models/reservation.rb
+++
@@ -1,7 +0,0 @@
-class Reservation < ApplicationRecord
-  belongs_to :clothingitem
-  belongs_to :user
-
-  validates :begin_date, presence: true
-  validates :end_date, presence: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root to: 'clothing_items#home'
-  resources :clothing_items, only: [:index, :show] do
-    resources :reservations, except: [:destroy]
-  end
-  resources :reservations, only: [:destroy]
+  resources :clothing_items, only: [:index, :show]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
