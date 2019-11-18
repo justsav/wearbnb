dd08b53aeb636c3f7a09bc3cfc1a6ebf61671ea4
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-18 16:12:56 +0000

    update seeds and migration

--- app/controllers/clothing_items_controller.rb
+++ app/controllers/clothing_items_controller.rb
@@ -1,10 +1,13 @@
 class ClothingItemsController < ApplicationController
   def index
+    @clothing_items = ClothingItem.all
   end
 
   def show
+    @clothing_item = ClothingItem.find(params[:id])
   end
 
   def home
+    @clothing_items = ClothingItem.all
   end
 end
