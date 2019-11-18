d05d35b82b5467204595bc717dc60655605b19e1
Justin Savage <justin.savage@live.com>
2019-11-18 14:52:27 +0000

    submit reservation model and controller v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'reservations/index'
+  get 'reservations/show'
+  get 'reservations/new'
+  get 'reservations/create'
+  get 'reservations/edit'
+  get 'reservations/update'
+  get 'reservations/destroy'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
