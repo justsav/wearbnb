3e42e384dbcf3650d57a6db6f32104742d483a6d
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-19 12:43:07 +0000

    changed headers font to body font

--- app/controllers/clothing_items_controller.rb
+++ app/controllers/clothing_items_controller.rb
@@ -1,13 +1,13 @@
 class ClothingItemsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :index]
+  skip_before_action :authenticate_user!, only: [:home, :index, :show]
 
   def index
-    raise
     @clothing_items = ClothingItem.all
   end
 
   def show
     @clothing_item = ClothingItem.find(params[:id])
+
   end
 
   def home
