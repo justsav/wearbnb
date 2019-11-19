6e15533dd5314b334c66902a749b45ac8130c2ce
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-19 19:04:49 +0000

    user can reserve clothes

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -11,9 +11,19 @@ class ReservationsController < ApplicationController
   end
 
   def create
+    @reservation = Reservation.new(review_params)
+    @clothing_item = ClothingItem.find(params[:clothing_item_id])
+    @user = current_user
     dates = params[:reservation][:begin_date].split('to')
     @begin_date = dates[0].strip
     @end_date = dates[1].strip
+
+    @reservation.clothing_item = @clothing_item
+    @reservation.user = @user
+    @reservation.begin_date = @begin_date
+    @reservation.end_date = @end_date
+    @reservation.save
+    redirect_to clothing_items_path
   end
 
   def edit
@@ -23,5 +33,11 @@ class ReservationsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:reservation).permit(:clothing_item_id)
   end
 end
