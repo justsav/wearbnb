2f7d5418609cbe3b2618dd1bb312dee8f8371245
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-22 11:15:03 +0000

    added status validations

--- db/migrate/20191122105450_add_status_to_reservations.rb
+++ db/migrate/20191122105450_add_status_to_reservations.rb
@@ -1,5 +1,5 @@
 class AddStatusToReservations < ActiveRecord::Migration[5.2]
   def change
-    add_column :reservations, :status, :string, default: 'pending'
+    add_column :reservations, :status, :string, default: 'pending', null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2019_11_22_105450) do
     t.datetime "end_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status", default: "pending"
+    t.string "status", default: "pending", null: false
     t.index ["clothing_item_id"], name: "index_reservations_on_clothing_item_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
