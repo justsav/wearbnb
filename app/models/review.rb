d45eb3dc2b892e3c8f1a36a84669c9878e334839
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-22 15:28:07 +0000

    added range of 1..5 to review

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ApplicationRecord
   belongs_to :user
 
   validates :rating, presence: true, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: (1..5).to_a }
 end
